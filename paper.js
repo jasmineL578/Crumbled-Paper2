93832665a82f6b9235dfb6b15bb7e606f4844df4
jasmineL578 <71908974+jasmineL578@users.noreply.github.com>
2020-12-19 21:40:54 -0800

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,22 @@
+class Paper {
+    constructor(x,y,radius) {
+      var options = { 
+        Static: false,
+        restitution:0.6,
+        density:0.5
+      }
+      this.body = Bodies.circle(x,y,radius,options);
+      this.radius = radius;
+      this.image=loadImage("paper.png")
+      World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.radius*2, this.radius*2);
+        pop();
+      }
+}
